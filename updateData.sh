536a8ab32243bf06d3dacdcdf374fdba9bdb95c2
Greg Dorrell <Greg@memimo.co.uk>
2012-01-28 22:37:27 +0000

    Updated some more

--- updateData.sh
+++ updateData.sh
@@ -6,3 +6,8 @@ wget -O ./resources/reddit.json "http://www.reddit.com/user/Grogs.json"
 
 #fresh twitter data
 wget -O ./resources/twitter.json "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=grogs&count=10" --no-check-certificate
+
+#get facebook data
+access_token=AAABuwSzpNoUBADNvOZCls36CxjSYnJbI2icCkz8gu5UbcE2mnXPU405WJyU1eRv6mr8G6rOUBndrNnZAp0i5ri0ZBCfrhEZD
+url="https://graph.facebook.com/me/feed?access_token=$access_token"
+wget -O ./resources/facebook.json $url --no-check-certificate
